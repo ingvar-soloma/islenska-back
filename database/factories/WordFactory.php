c24032be8bb1b580a0cd4e8b1c0677057f2128db
Ingvar <ingvar.soloma@gmail.com>
2024-07-26 23:35:30 +0000

    word api controller

--- database/factories/WordFactory.php
+++ database/factories/WordFactory.php
@@ -2,19 +2,25 @@
 
 namespace Database\Factories;
 
+use App\Models\Language;
 use App\Models\Word;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class WordFactory extends Factory
 {
+    private static array|null $languageIds = null;
     protected $model = Word::class;
 
     final public function definition(): array
     {
-        $this->faker->locale = 'is_IS'; // Set the locale to Icelandic
+        if (self::$languageIds === null) {
+            self::$languageIds = Language::all('id')->pluck('id')->toArray();
+        }
+
 
         return [
             'name' => $this->faker->word,
+            'language_id' => $this->faker->randomElement(self::$languageIds),
             'created_at' => $this->faker->dateTime,
             'updated_at' => $this->faker->dateTime,
         ];
